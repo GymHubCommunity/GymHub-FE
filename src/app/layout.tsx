42e82aa7b109426ae9c2601bd511bbe4ec72eae7
hanseulhee <3021062@gmail.com>
2024-01-14 23:39:07 +0900

    [#1] env: prettier 세팅

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
-import type { Metadata } from "next";
-import { ReactNode } from "react";
+import type { Metadata } from 'next';
+import { ReactNode } from 'react';
 
 export const metadata: Metadata = {
-  title: "GymHub",
-  description: "GymHub",
+  title: 'GymHub',
+  description: 'GymHub',
 };
 
 function RootLayout({ children }: { children: ReactNode }) {
