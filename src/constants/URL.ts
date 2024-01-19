43b7597133bc69ec15496cc83372d0c506280193
남궁수영 <sp38013@naver.com>
2024-01-19 12:17:16 +0900

    [#5] feat : metaData , URL 상수 추가

---
+++ src/constants/URL.ts
@@ -0,0 +1,3 @@
+const BASE_URL = 'http://localhost:3000/'; //임시 URL
+
+export { BASE_URL };
---
+++ src/constants/metaData.ts
@@ -0,0 +1,14 @@
+import { BASE_URL } from '@/constants/URL';
+import logo from '@/public/icons/logo.svg';
+import mainLogo from '@/public/images/mainLogo.png';
+
+const METADATA = {
+  TITLE: 'GymHub',
+  DESCRIPTION: '운동에 대해 이야기 나눠 보고싶다면, GymHub 에서!',
+  IMAGE: mainLogo.src,
+  URL: BASE_URL,
+  LOCALE: 'ko_KR',
+  ICON: logo.src,
+};
+
+export default METADATA;
