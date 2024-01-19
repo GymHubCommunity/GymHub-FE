b7ee1affac85c255bc052b3a71eb3200ae304520
oauch <lgm9515@naver.com>
2024-01-19 21:23:24 +0900

    [#17] refactor: dsn값 env로 관리

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -1,6 +1,6 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://03231d2ce57a7f12c015403dffd08f22@o4506597156978688.ingest.sentry.io/4506597398020096",
+  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
   tracesSampleRate: 1.0,
 });
