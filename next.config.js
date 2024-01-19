88a561383654a730b9e9244566645b018176ab82
oauch <lgm9515@naver.com>
2024-01-19 20:35:24 +0900

    [#17] Feat: Sentry 설정

--- next.config.js
+++ next.config.js
@@ -1,10 +1,19 @@
 /** @type {import('next').NextConfig} */
 const path = require('path');
+const { withSentryConfig } = require('@sentry/nextjs')
 
 const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
+  sentry: {
+    hideSourceMaps: true,
+  },
 };
 
-module.exports = nextConfig;
+const sentryWebpackPluginOptions = {
+  silent: true,
+  authToken: process.env.NEXT_PUBLIC_SENTRY_AUTH_TOKEN,
+}
+
+module.exports = withSentryConfig(nextConfig, sentryWebpackPluginOptions)
---
+++ sentry.client.config.ts
@@ -0,0 +1,7 @@
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://03231d2ce57a7f12c015403dffd08f22@o4506597156978688.ingest.sentry.io/4506597398020096",
+  tracesSampleRate: 1.0,
+
+})
