cf3472e9140e5472d23eed90609be736f8adaeef
oauch <lgm9515@naver.com>
2024-01-19 22:22:08 +0900

    [#17] fix: require 제거

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from 'next';
 import { ReactNode } from 'react';
 import { Toaster } from 'react-hot-toast';
-require('@/utils/sentry');
 
 export const metadata: Metadata = {
   title: 'GymHub',
