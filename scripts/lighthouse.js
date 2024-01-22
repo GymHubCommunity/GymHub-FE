6f19724a7b0570467c4d1384c4f834c27d806618
hanseulhee <3021062@gmail.com>
2024-01-22 18:19:39 +0900

    [#22] feat: lighthouse script 생성 및 pr 댓글 리포트 적용

---
+++ scripts/lighthouse.js
@@ -0,0 +1,45 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+module.exports = () => {
+  const fs = require('fs');
+
+  const score = (res) => (res >= 90 ? '🟢' : res >= 50 ? '🟠' : '🔴');
+  const formatResult = (res) => Math.round(res * 100);
+
+  const results = JSON.parse(fs.readFileSync('./lhci_reports/manifest.json'));
+
+  let comments = '';
+
+  results.forEach((result) => {
+    const { url, summary, jsonPath } = result;
+    const details = JSON.parse(fs.readFileSync(jsonPath));
+
+    const { audits } = details;
+
+    Object.keys(summary).forEach(
+      (key) => (summary[key] = formatResult(summary[key]))
+    );
+
+    const [, pageName] = url.split('3000');
+
+    const comment = [
+      `🏋🏻 ${pageName}의 Lighthouse 리포트입니다 🏋🏻 `,
+      `| Category | Score |`,
+      `| --- | --- |`,
+      `| ${score(summary.performance)} Performance | ${summary.performance} |`,
+    ].join('\n');
+
+    const detail = [
+      `| Category | Score |`,
+      `| --- | --- |`,
+      `| ${score(
+        audits['first-contentful-paint'].score * 100
+      )} First Contentful Paint | ${
+        audits['first-contentful-paint'].displayValue
+      } |`,
+    ].join('\n');
+
+    comments += `${comment}\n\n${detail}\n\n`;
+  });
+
+  return comments;
+};
