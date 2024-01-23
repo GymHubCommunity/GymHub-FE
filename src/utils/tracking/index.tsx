9fd785afa0c8941c8ae42b36b0fbb5e7eff59ee2
남궁수영 <sp38013@naver.com>
2024-01-23 20:53:25 +0900

    [#35] feat : Tracking 컴포넌트

---
+++ src/utils/tracking/index.tsx
@@ -0,0 +1,11 @@
+import GoogleAnalytics from '@/utils/tracking/googleAnalytics';
+
+function Tracking() {
+  return (
+    <>
+      <GoogleAnalytics />
+    </>
+  );
+}
+
+export default Tracking;
