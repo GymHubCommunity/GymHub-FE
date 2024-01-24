8f1aa93b4f842419251c1318eb07dd0027e7ed7a
hanseulhee <3021062@gmail.com>
2024-01-24 20:16:10 +0900

    [#43] env: 검사 속성 추가

--- scripts/lighthouse.js
+++ scripts/lighthouse.js
@@ -26,17 +26,45 @@ module.exports = () => {
       `| Category | Score |`,
       `| --- | --- |`,
       `| ${score(summary.performance)} Performance | ${summary.performance} |`,
+      `| ${score(summary.accessibility)} Accessibility | ${
+        summary.accessibility
+      } |`,
+      `| ${score(summary[`best-practices`])} Best Practices | ${
+        summary[`best-practices`]
+      } |`,
+      `| ${score(summary.seo)} SEO | ${summary.seo} |`,
+      `| ${score(summary.pwa)} PWA | ${summary.pwa} |`,
     ].join('\n');
 
     const detail = [
       `| Category | Score |`,
       `| --- | --- |`,
       `| ${score(
-        audits['first-contentful-paint'].score * 100
+        audits[`first-contentful-paint`].score * 100
       )} First Contentful Paint | ${
-        audits['first-contentful-paint'].displayValue
+        audits[`first-contentful-paint`].displayValue
       } |`,
-    ].join('\n');
+      `| ${score(
+        audits[`speed-index`].score * 100
+      )} Speed Index | ${
+        audits[`speed-index`].displayValue
+      } |`,
+      `| ${score(
+        audits[`total-blocking-time`].score * 100
+      )} Total Blocking Time | ${
+        audits[`total-blocking-time`].displayValue
+      } |`,
+      `| ${score(
+        audits[`largest-contentful-paint`].score * 100
+      )} Largest Contentful Paint | ${
+        audits[`largest-contentful-paint`].displayValue
+      } |`,
+      `| ${score(
+        audits[`cumulative-layout-shift`].score * 100
+      )} Cumulative Layout Shift | ${
+        audits[`cumulative-layout-shift`].displayValue
+      } |`
+    ].join("\n");
 
     comments += `${comment}\n\n${detail}\n\n`;
   });
