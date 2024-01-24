db1e04273482ab3a5ba42ebc8363f404a3448ab1
hanseulhee <3021062@gmail.com>
2024-01-24 20:35:09 +0900

    [#43] env: forEach 제거

--- scripts/lighthouse.js
+++ scripts/lighthouse.js
@@ -9,61 +9,59 @@ module.exports = () => {
 
   let comments = '';
 
-  results.forEach((result) => {
-    const { summary, jsonPath } = result[0];
-    const details = JSON.parse(fs.readFileSync(jsonPath));
+  const { summary, jsonPath } = results[0];
+  const details = JSON.parse(fs.readFileSync(jsonPath));
 
-    const { audits } = details;
+  const { audits } = details;
 
-    Object.keys(summary).forEach(
-      (key) => (summary[key] = formatResult(summary[key]))
-    );
+  Object.keys(summary).forEach(
+    (key) => (summary[key] = formatResult(summary[key]))
+  );
 
-    const [, pageName] = url.split('3000');
+  const [, pageName] = url.split('3000');
 
-    const comment = [
-      `🏋🏻 Gymhub의 Lighthouse 리포트입니다 🏋🏻 `,
-      `| Category | Score |`,
-      `| --- | --- |`,
-      `| ${score(summary.performance)} Performance | ${summary.performance} |`,
-      `| ${score(summary.accessibility)} Accessibility | ${
-        summary.accessibility
-      } |`,
-      `| ${score(summary[`best-practices`])} Best Practices | ${
-        summary[`best-practices`]
-      } |`,
-      `| ${score(summary.seo)} SEO | ${summary.seo} |`,
-      `| ${score(summary.pwa)} PWA | ${summary.pwa} |`,
-    ].join('\n');
+  const comment = [
+    `🏋🏻 Gymhub의 Lighthouse 리포트입니다 🏋🏻 `,
+    `| Category | Score |`,
+    `| --- | --- |`,
+    `| ${score(summary.performance)} Performance | ${summary.performance} |`,
+    `| ${score(summary.accessibility)} Accessibility | ${
+      summary.accessibility
+    } |`,
+    `| ${score(summary[`best-practices`])} Best Practices | ${
+      summary[`best-practices`]
+    } |`,
+    `| ${score(summary.seo)} SEO | ${summary.seo} |`,
+    `| ${score(summary.pwa)} PWA | ${summary.pwa} |`,
+  ].join('\n');
 
-    const detail = [
-      `| Category | Score |`,
-      `| --- | --- |`,
-      `| ${score(
-        audits[`first-contentful-paint`].score * 100
-      )} First Contentful Paint | ${
-        audits[`first-contentful-paint`].displayValue
-      } |`,
-      `| ${score(audits[`speed-index`].score * 100)} Speed Index | ${
-        audits[`speed-index`].displayValue
-      } |`,
-      `| ${score(
-        audits[`total-blocking-time`].score * 100
-      )} Total Blocking Time | ${audits[`total-blocking-time`].displayValue} |`,
-      `| ${score(
-        audits[`largest-contentful-paint`].score * 100
-      )} Largest Contentful Paint | ${
-        audits[`largest-contentful-paint`].displayValue
-      } |`,
-      `| ${score(
-        audits[`cumulative-layout-shift`].score * 100
-      )} Cumulative Layout Shift | ${
-        audits[`cumulative-layout-shift`].displayValue
-      } |`,
-    ].join('\n');
+  const detail = [
+    `| Category | Score |`,
+    `| --- | --- |`,
+    `| ${score(
+      audits[`first-contentful-paint`].score * 100
+    )} First Contentful Paint | ${
+      audits[`first-contentful-paint`].displayValue
+    } |`,
+    `| ${score(audits[`speed-index`].score * 100)} Speed Index | ${
+      audits[`speed-index`].displayValue
+    } |`,
+    `| ${score(
+      audits[`total-blocking-time`].score * 100
+    )} Total Blocking Time | ${audits[`total-blocking-time`].displayValue} |`,
+    `| ${score(
+      audits[`largest-contentful-paint`].score * 100
+    )} Largest Contentful Paint | ${
+      audits[`largest-contentful-paint`].displayValue
+    } |`,
+    `| ${score(
+      audits[`cumulative-layout-shift`].score * 100
+    )} Cumulative Layout Shift | ${
+      audits[`cumulative-layout-shift`].displayValue
+    } |`,
+  ].join('\n');
 
-    comments += `${comment}\n\n${detail}\n\n`;
-  });
+  comments += `${comment}\n\n${detail}\n\n`;
 
   return comments;
 };
