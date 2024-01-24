d0a45c92111b3034eaf1b9f1dcbe3d6762089bd5
남궁수영 <sp38013@naver.com>
2024-01-24 17:46:23 +0900

    [#35] feat : mixpanel 설정

--- src/utils/tracking/index.tsx
+++ src/utils/tracking/index.tsx
@@ -1,11 +1,13 @@
 import GoogleAnalytics from '@/utils/tracking/googleAnalytics';
 import Hotjar from '@/utils/tracking/hotjar';
+import MixPanel from '@/utils/tracking/mixpanel';
 
 function Tracking() {
   return (
     <>
       <GoogleAnalytics />
       <Hotjar />
+      <MixPanel />
     </>
   );
 }
