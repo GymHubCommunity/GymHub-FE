4b52a7388abbebb9479d194a7bed32930111c8c3
miniposi <rndtn6851@gmail.com>
2024-01-25 15:42:26 +0900

    [#27] feat: clean-webpack-plugin 속성 추가

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,23 @@ const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 const BundleAnalyzerPlugin =
   require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   mode: 'production',
   entry: './src/app/page.tsx',
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
   optimization: {
     minimize: true,
     minimizer: [
@@ -23,5 +35,5 @@ module.exports = {
       chunks: 'all',
     },
   },
-  plugins: [new BundleAnalyzerPlugin()],
+  plugins: [new BundleAnalyzerPlugin(), new CleanWebpackPlugin()],
 };
