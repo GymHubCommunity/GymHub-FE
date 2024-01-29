9cdf45cce5704464cc57e3e297bdad0fbcede6dc
miniposi <rndtn6851@gmail.com>
2024-01-29 10:13:48 +0900

    [#27] fix: babel runtime 수정

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,8 @@
 module.exports = {
-  presets: ['@babel/preset-react', '@babel/preset-typescript'],
+  presets: [
+    '@babel/preset-react',
+    '@babel/preset-typescript',
+    ,
+    { runtime: 'automatic' },
+  ],
 };
