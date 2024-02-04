fbc64128a90f1a227aa24e2dbbb143ea1b5b688c
oauch <lgm9515@naver.com>
2024-02-04 15:34:36 +0900

    [#97] feat: Profile 컴포넌트

--- src/components/molecules/Profile/index.tsx
+++ src/components/molecules/Profile/index.tsx
@@ -1,17 +1,26 @@
-import Name from '@/components/atoms/Name';
+import Info from '@/components/atoms/CountInfo';
 import ProfileImg from '@/components/atoms/ProfileImg';
-import WorkOutDays from '@/components/atoms/WorkOutDays';
 import styles from '@/components/molecules/Profile/Profile.module.scss';
-import Patrick from '@/public/images/star.jpeg';
+import { StaticImageData } from 'next/image';
 
-function Profile() {
+interface ProfileProps {
+  data: {
+    imgUrl: StaticImageData;
+    postCount: string;
+    workOutCount: string;
+    followCount: string;
+    followingCount: string;
+  };
+}
+
+function Profile({ data }: ProfileProps) {
   return (
     <div className={styles.wrapper}>
-      <ProfileImg imgUrl={Patrick} />
-      <div className={styles.inWrapper}>
-        <Name name="뚱이" />
-        <WorkOutDays date="200" />
-      </div>
+      <ProfileImg imgUrl={data.imgUrl} size={67} />
+      <Info type="normal" content="게시글" count={data.postCount} />
+      <Info type="normal" content="운동 일수" count={data.workOutCount} />
+      <Info type="follow" content="팔로우" count={data.followCount} />
+      <Info type="follow" content="팔로잉" count={data.followingCount} />
     </div>
   );
 }
