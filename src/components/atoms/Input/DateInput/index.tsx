b6e29a9d6390e35d31ceb75d206476e4f626c375
miniposi <rndtn6851@gmail.com>
2024-02-05 17:57:38 +0900

    [#72] fix: 불필요한 값들 제외

--- src/components/atoms/Input/DateInput/index.tsx
+++ src/components/atoms/Input/DateInput/index.tsx
@@ -1,35 +1,25 @@
 'use client';
 import RegisterItem from '@/components/Form/RegisterItem';
-import RegisterFormProvider from '@/components/FormProvider/RegisterFormProvider';
-import { SchemaProps } from '@/types/user';
 import DateFormat from '@/utils/DateFormat';
 import { useState } from 'react';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
-import { useFormContext } from 'react-hook-form';
 
 function DateInput() {
   const { year, month, day } = DateFormat(new Date());
   const minDateTime = year + '-' + month + '-' + day;
   const [selectedDate, setSelectedDate] = useState<Date | null>(new Date());
 
-  const {
-    register,
-    formState: { errors },
-  } = useFormContext<SchemaProps>();
-
   return (
-    <RegisterFormProvider>
-      <RegisterItem title="날짜 " required>
-        <DatePicker
-          dateFormat="yyyy.MM.dd"
-          shouldCloseOnSelect
-          minDate={new Date(minDateTime)}
-          selected={selectedDate}
-          onChange={(date) => setSelectedDate(date)}
-        />
-      </RegisterItem>
-    </RegisterFormProvider>
+    <RegisterItem title="날짜 " required>
+      <DatePicker
+        dateFormat="yyyy.MM.dd"
+        shouldCloseOnSelect
+        minDate={new Date(minDateTime)}
+        selected={selectedDate}
+        onChange={(date) => setSelectedDate(date)}
+      />
+    </RegisterItem>
   );
 }
 
