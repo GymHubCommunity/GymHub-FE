7202dba229d6c44dcd62e879eff8fca7206c74a2
oauch <lgm9515@naver.com>
2024-02-05 18:25:26 +0900

    [#87] feat: props로 size 설정

--- src/components/atoms/Slider/index.tsx
+++ src/components/atoms/Slider/index.tsx
@@ -1,28 +1,39 @@
 import styles from '@/components/atoms/Slider/Slider.module.scss';
-import SlideItems from '@/constants/SlideItems';
 
-import Image from 'next/image';
+import Image, { StaticImageData } from 'next/image';
 import 'swiper/css';
 import 'swiper/css/pagination';
 import { Pagination } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
-function Slider() {
+export interface imgProps {
+  id?: number;
+  imgUrl: StaticImageData; // TODO: string으로 타입 변경에정
+}
+
+interface SliderProps {
+  size: 'default' | 'large';
+  imgItems: imgProps[];
+}
+
+function Slider({ size = 'default', imgItems }: SliderProps) {
   return (
     <Swiper
       pagination={{
         clickable: true,
       }}
       modules={[Pagination]}
-      className={styles.swiper}
+      className={size === 'default' ? styles.swiper : styles.large}
     >
-      {SlideItems.slideItems.map((val) => (
+      {imgItems.map((val) => (
         <SwiperSlide key={val.id}>
           <Image
             fill
-            objectFit="contain"
+            sizes="100%"
+            priority={true}
             src={val.imgUrl}
             alt="슬라이드 이미지"
+            className={styles.img}
           />
         </SwiperSlide>
       ))}
