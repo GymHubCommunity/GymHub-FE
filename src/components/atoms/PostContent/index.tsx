94ae53b0280378ede32be9b07d7d9038a434a678
oauch <lgm9515@naver.com>
2024-02-05 16:57:26 +0900

    [#106] feat: PostContent 컴포넌트 검색 관련 내용 추가

--- src/components/atoms/PostContent/index.tsx
+++ src/components/atoms/PostContent/index.tsx
@@ -1,25 +1,28 @@
-import styles from '@/components/atoms/Content/Content.module.scss';
+import styles from '@/components/atoms/PostContent/PostContent.module.scss';
 
 interface PostContentProps {
-  type: 'default' | 'workOut';
+  type: 'default' | 'workOut' | 'search';
   content?: string;
   workOutTime?: string;
+  searchCount?: number;
 }
 
 function PostContent({
   type = 'default',
   content,
   workOutTime,
+  searchCount,
 }: PostContentProps) {
-  return (
-    <>
-      {type === 'workOut' ? (
+  switch (type) {
+    case 'workOut':
+      return (
         <p className={styles.content}>오늘 {workOutTime}시간 전 운동 완료!</p>
-      ) : (
-        <p className={styles.content}>{content}</p>
-      )}
-    </>
-  );
+      );
+    case 'search':
+      return <p className={styles.content}>검색결과 {searchCount}건</p>;
+    default:
+      return <p className={styles.content}>{content}</p>;
+  }
 }
 
 export default PostContent;
