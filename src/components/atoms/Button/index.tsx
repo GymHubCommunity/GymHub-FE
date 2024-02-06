851e9998c387979d1806408f967e6d6946c04d48
hanseulhee <3021062@gmail.com>
2024-02-06 15:41:35 +0900

    [#116] feat: text classnames 적용

--- src/components/atoms/Button/index.tsx
+++ src/components/atoms/Button/index.tsx
@@ -1,24 +1,23 @@
 import styles from '@/components/atoms/Button/Button.module.scss';
 import classNames from 'classnames/bind';
-import { ReactNode } from 'react';
 
 const cn = classNames.bind(styles);
 
 interface ButtonProps {
   onClick: () => void;
-  children: ReactNode;
+  children: string;
   size: 'sm' | 'md' | 'lg';
   color: string;
 }
 
-function Button({ onClick, children, size, color }: ButtonProps) {
+function Button({ onClick, content, size, color }: ButtonProps) {
   return (
     <button
       type="button"
       onClick={onClick}
       className={cn('button', size, color)}
     >
-      {children}
+      {content}
     </button>
   );
 }
