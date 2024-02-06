caddbe75a4ad7ebd427c6bab4050385cab2ad901
hanseulhee <3021062@gmail.com>
2024-02-06 15:32:07 +0900

    [#116] feat: text component 개발

--- src/components/atoms/Button/index.tsx
+++ src/components/atoms/Button/index.tsx
@@ -1,23 +1,24 @@
 import styles from '@/components/atoms/Button/Button.module.scss';
 import classNames from 'classnames/bind';
+import { ReactNode } from 'react';
 
 const cn = classNames.bind(styles);
 
 interface ButtonProps {
   onClick: () => void;
-  content: string;
+  children: ReactNode;
   size: 'sm' | 'md' | 'lg';
   color: string;
 }
 
-function Button({ onClick, content, size, color }: ButtonProps) {
+function Button({ onClick, children, size, color }: ButtonProps) {
   return (
     <button
       type="button"
       onClick={onClick}
       className={cn('button', size, color)}
     >
-      {content}
+      {children}
     </button>
   );
 }
