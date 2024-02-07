597e224a86889797c1a674b36334427a7024aba5
남궁수영 <sp38013@naver.com>
2024-02-07 23:06:31 +0900

    [#102] feat : 페이지별 이름 상수처리

--- src/components/organisms/AddPost/index.tsx
+++ src/components/organisms/AddPost/index.tsx
@@ -3,11 +3,12 @@ import PostEditor from '@/components/atoms/Editor/PostEditor';
 import AttachButtons from '@/components/molecules/Post/AttachButtons';
 import styles from '@/components/organisms/AddPost/AddPost.module.scss';
 import BackButtonHeader from '@/components/organisms/Header/BackButtonHeader';
+import { PAGE_NAMES } from '@/constants/PageNames';
 
 function AddPost() {
   return (
     <>
-      <BackButtonHeader pageName="게시물 작성" />
+      <BackButtonHeader pageName={PAGE_NAMES.POST.ADD_POST} />
       <div className={styles.wrapper}>
         <PostEditor />
         <div className={styles.inWrapper}>
