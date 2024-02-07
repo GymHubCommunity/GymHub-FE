0d6bd068fff16ca36cda5c35630c01c0ffdaa9ec
hanseulhee <3021062@gmail.com>
2024-02-07 15:22:44 +0900

    [#64] feat: social login 예시

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,34 @@
+'use client';
+import { signIn, signOut, useSession } from 'next-auth/react';
+import Image from 'next/image';
+
 function Home() {
-  return <main>home</main>;
+  const { data: session } = useSession();
+  const userData = session?.user;
+
+  return (
+    <main>
+      {session ? (
+        <>
+          {userData?.email}
+          {userData?.image && (
+            <Image
+              src={userData.image}
+              alt="프로필 이미지"
+              width={200}
+              height={200}
+              priority
+            />
+          )}
+          <button onClick={() => signOut({ callbackUrl: '/' })}>
+            로그아웃
+          </button>
+        </>
+      ) : (
+        <button onClick={() => signIn()}>로그인</button>
+      )}
+    </main>
+  );
 }
 
 export default Home;
