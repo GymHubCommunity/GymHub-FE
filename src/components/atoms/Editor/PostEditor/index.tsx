0089868f9cb4db2ba6ce5583d9a8c6bd54774238
남궁수영 <sp38013@naver.com>
2024-02-07 23:12:55 +0900

    [#102] stlye : editor  placehoder 줄바꿈 적용

--- src/components/atoms/Editor/PostEditor/index.tsx
+++ src/components/atoms/Editor/PostEditor/index.tsx
@@ -2,11 +2,10 @@ import styles from '@/components/atoms/Editor/PostEditor/PostEditor.module.scss'
 
 //TODO: 기능 작업 해야함
 function PostEditor() {
+  const placeHolder =
+    '오늘은 어떤 얘기를 할까요?\n운동 루틴, 식단, 다이어트 등등...';
   return (
-    <textarea
-      className={styles.editor}
-      placeholder="오늘은 어떤 이야기를 할까요?"
-    ></textarea>
+    <textarea className={styles.editor} placeholder={placeHolder}></textarea>
   );
 }
 
