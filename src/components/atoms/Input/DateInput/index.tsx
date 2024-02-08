b809f8675b913100a531f87e4182a70be467aacd
miniposi <rndtn6851@gmail.com>
2024-02-08 20:55:35 +0900

    [#133] feat: 날짜 입력 폼 커스텀

---
+++ src/components/atoms/Input/DateInput/index.tsx
@@ -0,0 +1,62 @@
+import RegisterItem from '@/components/atoms/Form/RegisterItem';
+import DateFormat from '@/utils/DateFormat';
+import { useState } from 'react';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import styles from '@/components/atoms/Input/DateInput/DataInput.module.scss';
+import LeftArrowSvg from '@/assets/icons/LeftArrorSvg';
+import RightArrowSvg from '@/assets/icons/RightArrowSvg';
+import { ko } from 'date-fns/locale';
+
+function DateInput() {
+  const { year, month, day } = DateFormat(new Date());
+  const minDateTime = year + '-' + month + '-' + day;
+  const [selectedDate, setSelectedDate] = useState<Date | null>(new Date());
+
+  return (
+    <RegisterItem>
+      <div className={styles.wrapper}>
+        <DatePicker
+          inline
+          dateFormat="yyyy.MM.dd"
+          calendarStartDay={1}
+          shouldCloseOnSelect
+          minDate={new Date(minDateTime)}
+          selected={selectedDate}
+          className={styles.input}
+          calendarClassName={styles.calender}
+          dayClassName={(d) =>
+            d.getDate() === selectedDate!.getDate()
+              ? styles.selectedDay
+              : styles.unselectedDay
+          }
+          locale={ko}
+          onChange={(date) => setSelectedDate(date)}
+          renderCustomHeader={({ decreaseMonth, increaseMonth }) => (
+            <div className={styles.header}>
+              <button
+                type="button"
+                onClick={decreaseMonth}
+                className={styles.monthButton}
+              >
+                <LeftArrowSvg />
+              </button>
+              <div className={styles.dateWrapper}>
+                {year}. {month}월
+              </div>
+              <button
+                type="button"
+                onClick={increaseMonth}
+                className={styles.monthButton}
+              >
+                <RightArrowSvg />
+              </button>
+            </div>
+          )}
+        />
+      </div>
+    </RegisterItem>
+  );
+}
+
+export default DateInput;
