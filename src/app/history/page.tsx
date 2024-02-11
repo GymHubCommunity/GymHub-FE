d58c4aa92649d156dbffaec16fc3cf880b09e049
miniposi <rndtn6851@gmail.com>
2024-02-12 01:28:10 +0900

    [#133] feat: history 조립

--- src/app/history/page.tsx
+++ src/app/history/page.tsx
@@ -4,14 +4,20 @@ import DateInput from '@/components/atoms/Input/DateInput';
 import DailyHistory from '@/components/molecules/DailyHistory';
 import useToggleMenu from '@/hooks/useToggleMenu';
 import styles from '@/app/history/HIstoryPage.module.scss';
+import Text from '@/components/atoms/Text';
+import AddTrackButton from '@/components/atoms/Button/AddTrackButton';
 
 function HistoryPage() {
   const { isOpen } = useToggleMenu();
   return (
-    <div>
+    <div className={styles.wrapper}>
       {isOpen && <div className={styles.blur}></div>}
+      <div className={styles.header}>
+        <Text>운동 기록</Text>
+      </div>
       <DateInput />
       <DailyHistory />
+      <AddTrackButton />
     </div>
   );
 }
