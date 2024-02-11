0bc3847ede0973c7c2d554157eec84bc9345baa7
hanseulhee <3021062@gmail.com>
2024-02-11 22:04:24 +0900

    [#127] feat: provider each icon 적용

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,15 @@
-function Home() {
-  return <main>home</main>;
+import { getServerSession } from 'next-auth';
+import authOptions from '@/app/api/auth/[...nextauth]/options';
+
+async function Home() {
+  const session = await getServerSession(authOptions);
+
+
+  return (
+    <main>
+      <h1>{JSON.stringify(session)}</h1>
+    </main>
+  );
 }
 
 export default Home;
--- src/components/atoms/Button/LoginButton/index.tsx
+++ src/components/atoms/Button/LoginButton/index.tsx
@@ -1,9 +1,11 @@
 'use client';
 import styles from '@/components/atoms/Button/LoginButton/LoginButton.module.scss';
-import classNames from 'classnames/bind';;
+import classNames from 'classnames/bind';
 import Text from '@/components/atoms/Text';
 import { BuiltInProviderType } from 'next-auth/providers/index';
 import { ClientSafeProvider, LiteralUnion, signIn } from 'next-auth/react';
+import GoogleIconSvg from '@/assets/icons/GoogleIconSvg';
+import KakaoIconSvg from '@/assets/icons/KakaoIconSvg';
 
 const cn = classNames.bind(styles);
 
@@ -22,12 +24,12 @@ function LoginButton({ providers }: ProviderProps) {
           type="button"
           onClick={() => signIn(provider.id, { callbackUrl: '/' })}
           key={provider.name}
-          className={cn('button')}
+          className={cn('button', `${provider.id}`)}
         >
+          {provider.name == 'Kakao' ? <KakaoIconSvg /> : <GoogleIconSvg />}
           <Text button="button">{provider.name}로 시작하기</Text>
         </button>
       ))}
-      ;
     </>
   );
 }
