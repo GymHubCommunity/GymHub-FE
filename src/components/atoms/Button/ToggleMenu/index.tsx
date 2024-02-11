27608defcf6bed748b3a47a2d69af822c999281b
miniposi <rndtn6851@gmail.com>
2024-02-12 01:28:23 +0900

    [#133] feat: 페이지 이동 추가

--- src/components/atoms/Button/ToggleMenu/index.tsx
+++ src/components/atoms/Button/ToggleMenu/index.tsx
@@ -3,6 +3,7 @@ import styles from '@/components/atoms/Button/ToggleMenu/ToggleMenu.module.scss'
 import { profileItems, postItems, historyItems } from '@/constants/ToggleMenu';
 import useToggleMenu from '@/hooks/useToggleMenu';
 import classNames from 'classnames/bind';
+import { useRouter } from 'next/navigation';
 
 const cn = classNames.bind(styles);
 
@@ -11,6 +12,7 @@ interface ToggleMenuProp {
 }
 
 function ToggleMenu({ type }: ToggleMenuProp) {
+  const router = useRouter();
   const { isOpen, openMenu, closeMenu } = useToggleMenu();
   const menuItems =
     type === 'profile'
@@ -18,6 +20,16 @@ function ToggleMenu({ type }: ToggleMenuProp) {
       : type === 'post'
         ? postItems
         : historyItems;
+
+  const handleOnClick = (id: number) => {
+    if (id === 0) {
+      router.push('/history/edit');
+    } else if (id === 1) {
+      // TODO
+    } else {
+      // modal
+    }
+  };
 
   return (
     <div role="presentation" onBlur={closeMenu}>
@@ -31,7 +43,7 @@ function ToggleMenu({ type }: ToggleMenuProp) {
               role="presentation"
               key={val.id}
               className={styles.itemWrapper}
-              onMouseDown={() => console.log(val.item)}
+              onMouseDown={() => handleOnClick(val.id)}
             >
               <div className={cn('item', { delete: val.id === 2 })}>
                 {val.item}
