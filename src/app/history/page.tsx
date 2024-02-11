5b5b96f3daf8279971bbcbc1e46024ca6798c2fa
miniposi <rndtn6851@gmail.com>
2024-02-11 20:13:20 +0900

    [#133] feat: modal 배경 블러 처리

--- src/app/history/page.tsx
+++ src/app/history/page.tsx
@@ -1,9 +1,19 @@
 'use client';
 
 import DateInput from '@/components/atoms/Input/DateInput';
+import DailyHistory from '@/components/molecules/DailyHistory';
+import useToggleMenu from '@/hooks/useToggleMenu';
+import styles from '@/app/history/HIstoryPage.module.scss';
 
 function HistoryPage() {
-  return <DateInput />;
+  const { isOpen } = useToggleMenu();
+  return (
+    <div>
+      {isOpen && <div className={styles.blur}></div>}
+      <DateInput />
+      <DailyHistory />
+    </div>
+  );
 }
 
 export default HistoryPage;
