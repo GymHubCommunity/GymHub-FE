514467b657217babd300b4c848887efa77057f68
hanseulhee <3021062@gmail.com>
2024-02-11 23:16:29 +0900

    [#127] feat: main page 예시 삭제

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,5 @@
-import { getServerSession } from 'next-auth';
-import authOptions from '@/app/api/auth/[...nextauth]/options';
-
-async function Home() {
-  const session = await getServerSession(authOptions);
-
-  return (
-    <main>
-      <h1>{JSON.stringify(session)}</h1>
-    </main>
-  );
+function Home() {
+  return <main>home</main>;
 }
 
 export default Home;
