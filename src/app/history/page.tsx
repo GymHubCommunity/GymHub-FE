69b0998f722204f9ecb94f64043c415395d886c9
miniposi <rndtn6851@gmail.com>
2024-02-12 21:33:56 +0900

    [#133] feat: 운동 기록 페이지 organism 분리

--- src/app/history/page.tsx
+++ src/app/history/page.tsx
@@ -1,30 +1,9 @@
 'use client';
 
-import DateInput from '@/components/atoms/Input/DateInput';
-import DailyHistory from '@/components/molecules/DailyHistory';
-import useToggleMenu from '@/hooks/useToggleMenu';
-import styles from '@/app/history/HIstoryPage.module.scss';
-import Text from '@/components/atoms/Text';
-import AddTrackButton from '@/components/atoms/Button/AddTrackButton';
-import Modal from '@/components/organisms/Modal';
-import useModalInfo from '@/hooks/useModalInfo';
+import History from '@/components/organisms/History';
 
 function HistoryPage() {
-  const { isOpen } = useToggleMenu();
-  const { isShow, closeModal } = useModalInfo();
-
-  return (
-    <div className={styles.wrapper}>
-      {isOpen && <div className={styles.blur}></div>}
-      <div className={styles.header}>
-        <Text>운동 기록</Text>
-      </div>
-      <DateInput />
-      <DailyHistory />
-      <AddTrackButton />
-      {isShow && <Modal isShow={isShow} closeModal={closeModal} />}
-    </div>
-  );
+  return <History />;
 }
 
 export default HistoryPage;
