dca05323d0e4e9c5364f71f28f171c670399bbf8
hanseulhee <3021062@gmail.com>
2024-02-13 01:32:33 +0900

    [#125] feat: register schema 작성

--- src/constants/userSchema.ts
+++ src/constants/userSchema.ts
@@ -1,4 +1,3 @@
-//TODO: 확정된 항목으로 수정
 import * as yup from 'yup';
 
 const EMAIL_STANDARD =
@@ -8,11 +7,18 @@ const userFormSchema = yup.object().shape({
   email: yup
     .string()
     .trim()
-    .required('이메일을 입력해주세요')
     .matches(EMAIL_STANDARD, '올바른 이메일 주소가 아닙니다'),
-  address: yup.string().required('주소를 입력해주세요'),
-  introduction: yup.string().required('한 줄 소개를 입력해주세요'),
-  status: yup.string().required('운동 상태를 선택해주세요'),
+  address: yup.string(),
+  introduction: yup.string(),
+  status: yup.string(),
+  nickname: yup
+    .string()
+    .max(12, '닉네임은 12글자 이내로 입력해주세요.')
+    .matches(
+      /^[0-9|a-z|A-Z|ㄱ-ㅎ|ㅏ-ㅣ|가-힣]*$/,
+      '특수문자, 띄어쓰기 없이 입력해주세요.',
+    )
+    .required(''),
 });
 
 export { userFormSchema };
