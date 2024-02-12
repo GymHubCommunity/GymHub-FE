3c2bcc48ee363eb1a55e73fe5cf5d1c1345a4d4b
hanseulhee <3021062@gmail.com>
2024-02-13 00:36:59 +0900

    [#125] feat: profile img 미리보기

--- src/components/molecules/Profile/ProfileImgSetting/index.tsx
+++ src/components/molecules/Profile/ProfileImgSetting/index.tsx
@@ -1,25 +1,53 @@
 import styles from '@/components/molecules/Profile/ProfileImgSetting/ProfileImgSetting.module.scss';
 import CameraSvg from '@/assets/icons/CameraSvg';
 import ProfileImgSvg from '@/assets/icons/ProfileImgSvg';
+import { ChangeEvent, useState } from 'react';
+import Image from 'next/image';
 
 function ProfileImgSetting() {
+  const [previewImage, setPreviewImage] = useState<string>('');
+
+  const handleChangeFile = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files) {
+      const file = e.target.files?.[0];
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+
+      reader.onload = (e) => {
+        if (typeof e.target?.result === 'string') {
+          setPreviewImage(e.target.result);
+        }
+      };
+    }
+  };
+
   return (
-    <div className={styles.wrapper}>
-      <div className={styles.profileImg}>
-        <ProfileImgSvg />
-      </div>
-      <div className={styles.cameraImg}>
-        <label htmlFor="profileImage">
-          <input
-            id="profileImage"
-            type="file"
-            accept="image/png, image/jpeg, image/jpg"
-            className={styles.cameraInput}
-          />
+    <label htmlFor="profileImage">
+      <div className={styles.wrapper}>
+        <input
+          id="profileImage"
+          type="file"
+          accept="image/png, image/jpeg, image/jpg"
+          className={styles.cameraInput}
+          onChange={handleChangeFile}
+        />
+        <div className={styles.profileImg}>
+          {previewImage ? (
+            <Image
+              src={previewImage}
+              alt="이미지 미리보기"
+              width={128}
+              height={128}
+            />
+          ) : (
+            <ProfileImgSvg />
+          )}
+        </div>
+        <div className={styles.cameraImg}>
           <CameraSvg />
-        </label>
+        </div>
       </div>
-    </div>
+    </label>
   );
 }
 
