0b4e1c9b1f11550df744b9fe4e8175f67b224efc
남궁수영 <sp38013@naver.com>
2024-02-15 20:19:47 +0900

    [#158] feat : pre-signedURL API 연동

--- src/components/atoms/Button/PictureButton/index.tsx
+++ src/components/atoms/Button/PictureButton/index.tsx
@@ -1,8 +1,11 @@
 import PictureSvg from '@/assets/icons/PictureSvg';
 import styles from '@/components/atoms/Button/PictureButton/PictureButton.module.scss';
 import { PictureButtonProps } from '@/types/image';
+import { instance, instanceFiles } from '@/apis';
+import { useEffect, useState } from 'react';
 
 function PictureButton({ onImageChange }: PictureButtonProps) {
+  const [file, setFile] = useState<File | null>(null);
   const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (!e.target.files) return;
     const file = e.target.files[0];
@@ -14,6 +17,26 @@ function PictureButton({ onImageChange }: PictureButtonProps) {
       }
     };
     e.target.value = '';
+    setFile(file);
+  };
+
+  useEffect(() => {
+    handleFileUpload();
+  }, [file]);
+
+  const handleFileUpload = async () => {
+    if (!file) return;
+
+    const formData = new FormData();
+    formData.append('contentLength', file.size + '');
+    formData.append('extension', file.type.replace('image/', ''));
+
+    try {
+      const response = await instance.post('/images/presigned_url', formData);
+      console.log('response  : ', response);
+    } catch (e) {
+      console.error(e);
+    }
   };
 
   return (
