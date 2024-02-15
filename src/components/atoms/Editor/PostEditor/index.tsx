e6940b2bdd71658b7457020c23807dd39f4a3452
남궁수영 <sp38013@naver.com>
2024-02-15 12:42:51 +0900

    [#158] feat : 게시글 react-hook-form

--- src/components/atoms/Editor/PostEditor/index.tsx
+++ src/components/atoms/Editor/PostEditor/index.tsx
@@ -1,11 +1,38 @@
 import styles from '@/components/atoms/Editor/PostEditor/PostEditor.module.scss';
+import { useFormContext } from 'react-hook-form';
 
 //TODO: 기능 작업 해야함
-function PostEditor() {
+
+interface PostEditorProps {
+  name: string;
+  handleChange: (event: React.ChangeEvent<HTMLTextAreaElement>) => void;
+}
+
+function PostEditor({ name, handleChange }: PostEditorProps) {
+  const {
+    register,
+    formState: { errors },
+  } = useFormContext();
   const placeHolder =
     '오늘은 어떤 얘기를 할까요?\n운동 루틴, 식단, 다이어트 등등...';
   return (
-    <textarea className={styles.editor} placeholder={placeHolder}></textarea>
+    <>
+      <textarea
+        id={name}
+        className={styles.editor}
+        placeholder={placeHolder}
+        {...register(name, {
+          required: {
+            value: true,
+            message: '글을 작성 해주세요.',
+          },
+        })}
+        onChange={handleChange}
+      ></textarea>
+      {errors[name] && (
+        <span className={styles.error}>This field is required</span>
+      )}
+    </>
   );
 }
 
--- src/components/organisms/AddPost/index.tsx
+++ src/components/organisms/AddPost/index.tsx
@@ -4,21 +4,37 @@ import AttachButtons from '@/components/molecules/Post/AttachButtons';
 import styles from '@/components/organisms/AddPost/AddPost.module.scss';
 import BackButtonHeader from '@/components/organisms/Header/BackButtonHeader';
 import { PAGE_NAMES } from '@/constants/PageNames';
+import { useState } from 'react';
+import { FormProvider, useForm } from 'react-hook-form';
 
 function AddPost() {
+  const [disabled, setDisabled] = useState(true);
+  const methods = useForm();
+
+  //TODO: content :string , imageURL: string[] , hashtags : string[] (값이 없을 경우 빈배열로 )
+  const onSubmit = (data: any) => {
+    // 폼 제출(submit) 로직을 여기에 구현합니다.
+    console.log('data : ', data);
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setDisabled(e.target.value.trim() === '');
+  };
   return (
     <>
       <BackButtonHeader pageName={PAGE_NAMES.POST.ADD_POST} />
-      <div className={styles.wrapper}>
-        <PostEditor />
-        <div className={styles.inWrapper}>
-          <AttachButtons />
-          <ConfirmButton
-            title="게시하기"
-            onClick={() => console.log('confirm')}
-          />
-        </div>
-      </div>
+      <FormProvider {...methods}>
+        <form
+          className={styles.wrapper}
+          onSubmit={methods.handleSubmit(onSubmit)}
+        >
+          <PostEditor name="content" handleChange={handleChange} />
+          <div className={styles.inWrapper}>
+            <AttachButtons />
+            <ConfirmButton title="게시하기" type="submit" disabled={disabled} />
+          </div>
+        </form>
+      </FormProvider>
     </>
   );
 }
