df55cf81466af8317aa9e8d6b4e542bb67d76110
hanseulhee <3021062@gmail.com>
2024-02-16 15:22:18 +0900

    [#125] feat: token cookies-next 적용

--- src/apis/user/register.ts
+++ src/apis/user/register.ts
@@ -1,9 +1,38 @@
 import { instance } from '@/apis';
 import { UserInputRegisterProps } from '@/types/user';
+import { deleteCookie, getCookie } from 'cookies-next';
 
 async function postRegister(body: UserInputRegisterProps) {
   const response = await instance.post(`/member/register`, body);
   return response;
 }
+
+export const authToken = {
+  access: (() => {
+    try {
+      return getCookie('accessToken');
+    } catch (err) {
+      return null;
+    }
+  })(),
+  refresh: (() => {
+    try {
+      return getCookie('refreshToken');
+    } catch (err) {
+      return null;
+    }
+  })(),
+  refetch: () => {
+    authToken.access = getCookie('accessToken');
+    authToken.refresh = getCookie('refreshToken');
+    return;
+  },
+  destroy: () => {
+    deleteCookie('accessToken');
+    deleteCookie('refreshToken');
+    authToken.access = null;
+    authToken.refresh = null;
+  },
+};
 
 export { postRegister };
