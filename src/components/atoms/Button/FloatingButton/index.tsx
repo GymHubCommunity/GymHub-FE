9b3819689032c33525f56deae3a3ce8d65fc34de
oauch <lgm9515@naver.com>
2024-02-17 18:44:12 +0900

    [#162] feat: FloatingButton type으로 구분

--- src/components/atoms/Button/FloatingButton/index.tsx
+++ src/components/atoms/Button/FloatingButton/index.tsx
@@ -1,12 +1,26 @@
+import FloatingDumbbellSvg from '@/assets/icons/FloatingDumbbellSvg';
 import PlusSvg from '@/assets/icons/PlusSvg';
 import styles from '@/components/atoms/Button/FloatingButton/FloatingButton.module.scss';
 import Link from 'next/link';
 
-function FloatingButton() {
+interface FloatingButtonProp {
+  type: 'post' | 'addExercise';
+}
+
+function FloatingButton({ type }: FloatingButtonProp) {
   return (
-    <Link href={'/post/add'} className={styles.wrapper}>
-      <PlusSvg />
-    </Link>
+    <>
+      {type === 'post' ? (
+        <Link href={'/post/add'} className={styles.wrapper}>
+          <PlusSvg />
+        </Link>
+      ) : (
+        // TODO: 운동 추가하기 버튼
+        <Link href={'/records/add'} className={styles.wrapper}>
+          <FloatingDumbbellSvg />
+        </Link>
+      )}
+    </>
   );
 }
 
