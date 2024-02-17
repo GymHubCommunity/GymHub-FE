943f27321482d724931a6e40355ba90d94689417
oauch <lgm9515@naver.com>
2024-02-17 18:54:56 +0900

    [#162] fix: sonarCloud 오류 처리

--- src/hooks/useToggleItems.ts
+++ src/hooks/useToggleItems.ts
@@ -13,20 +13,25 @@ function useToggleItems({ type }: ToggleMenuProp) {
   const router = useRouter();
   const pathName = usePathname();
   const { showModal } = useModalInfo();
-  const menuItems =
-    type === 'profile'
-      ? profileItems
-      : type === 'post'
-        ? postItems
-        : recordsItems;
+
+  let menuItems = [];
+
+  if (type === 'profile') menuItems = profileItems;
+  if (type === 'post') {
+    menuItems = postItems;
+  } else {
+    menuItems = recordsItems;
+  }
 
   const handleOnClick = (item: string) => {
     // TODO: 여기에 토글 메뉴에 관련한 기능 추가해주시면 됩니다.
     switch (item) {
       case POST_UPDATE:
         router.push('/records/[recordId]');
+        break;
       default:
         showModal();
+        break;
     }
   };
   return { pathName, menuItems, handleOnClick };
