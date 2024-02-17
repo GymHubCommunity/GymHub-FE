fb7faef5492058f357a9a4055a82c0fd7077d843
hanseulhee <3021062@gmail.com>
2024-02-18 00:06:14 +0900

    [#125] feat: cookie get 로직 주석

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,14 @@
 function Home() {
+  // const session = await getServerSession(authOptions);
+  // const cookieStore = cookies();
+
+  // const accessToken = cookieStore.get('accessToken');
+  // const refreshToken = cookieStore.get('refresh')?.value;
+
+  // if (refreshToken && !session) {
+  //   console.log('로그인해라');
+  // }
+
   return <main>home</main>;
 }
 
