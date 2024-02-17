74f5e31da4489d8b8adf801979938597f506a872
hanseulhee <3021062@gmail.com>
2024-02-18 00:05:59 +0900

    [#125] feat: interceptor 설정

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,11 +1,15 @@
 import { BASE_URL } from '@/constants/common';
-import axios from 'axios';
+import axios, { AxiosError, AxiosResponse } from 'axios';
+import { getCookie, setCookie } from 'cookies-next';
+import { authToken, postRefreshToken } from './user/register';
 
 const instance = axios.create({
   baseURL: BASE_URL,
   headers: {
     'Content-type': 'application/json',
+    Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
   },
+  timeout: 5000,
 });
 
 const instanceFiles = axios.create({
@@ -16,3 +20,39 @@ const instanceFiles = axios.create({
 });
 
 export { instance, instanceFiles };
+
+function responsefulfilledInterceptor(res: AxiosResponse) {
+  if (200 <= res.status && res.status < 300) {
+    return res;
+  }
+  return Promise.reject(res.data);
+}
+
+async function responseRejectedInterceptor(error: AxiosError) {
+  if (error.response?.status === 401) {
+    const refreshToken = getCookie('refresh') as string;
+
+    try {
+      const token = await postRefreshToken(refreshToken);
+
+      if (token) {
+        authToken.destroy();
+        setCookie('accessToken', token.data.accessToken);
+        setCookie('refresh', refreshToken);
+      }
+    } catch (e) {
+      authToken.destroy();
+      console.log(e);
+    }
+
+    await new Promise((res) => res);
+    return null;
+  }
+
+  return error;
+}
+
+instance.interceptors.response.use(
+  responsefulfilledInterceptor,
+  responseRejectedInterceptor,
+);
