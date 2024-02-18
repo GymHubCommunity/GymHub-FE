44a6a42bda2fd9ce799f2af33b2a0d97d129de3f
남궁수영 <sp38013@naver.com>
2024-02-18 17:38:08 +0900

    [#158] refactor : AWS S3 URL env로 관리

--- src/constants/common.ts
+++ src/constants/common.ts
@@ -3,7 +3,7 @@
 const BASE_URL = 'http://localhost:8080/';
 
 //TODO: AWS S3 임시 URL ,BUCKET_NAME 배포되면 수정 해야함.
-const AWS_S3_URL = 'http://52.79.247.31:4566/';
+const AWS_S3_URL = process.env.NEXT_PUBLIC_AWS_S3_URL;
 const S3_BUCKET_NAME = 'test/';
 
 export { BASE_URL, S3_BUCKET_NAME, AWS_S3_URL };
