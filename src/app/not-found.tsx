4f8765917964929ac883313a1fd633cc8f72d9d5
hanseulhee <3021062@gmail.com>
2024-02-19 22:07:52 +0900

    [#179] feat: 아이콘 적용

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,13 +2,19 @@ import ErrorInfo from '@/components/molecules/ErrorInfo';
 import Footer from '@/components/organisms/Footer';
 import BackButtonHeader from '@/components/organisms/Header/BackButtonHeader';
 import styles from '@/components/molecules/ErrorInfo/ErrorInfo.module.scss';
+import Image from 'next/image';
+import NotFoundImg from '@/public/images/NotFoundImg.png';
 
 function NotFound() {
   return (
     <>
       <div className={styles.headerWrapper}>
         <BackButtonHeader pageName="오류 발생" />
-        <ErrorInfo text="오류가 발생했습니다."></ErrorInfo>
+        <div className={styles.itemWrapper}>
+          <ErrorInfo text="오류가 발생했습니다.">
+            <Image src={NotFoundImg} alt="not found 이미지" priority />
+          </ErrorInfo>
+        </div>
       </div>
       <Footer />
     </>
