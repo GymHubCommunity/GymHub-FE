c9dd7ecad39b44d7bd13d5bd72c8c69e83986941
oauch <lgm9515@naver.com>
2024-02-20 15:15:28 +0900

    feat: Image 타입 지정

--- src/components/atoms/PostImg/index.tsx
+++ src/components/atoms/PostImg/index.tsx
@@ -1,17 +1,11 @@
 import styles from '@/components/atoms/PostImg/PostImg.module.scss';
 import { ImgProps } from '@/types/image';
 import Image from 'next/image';
-import { useRouter } from 'next/navigation';
-
-// TODO: 게시글 id 가져오기 (임시 데이터)
-const id = 32;
 
 function PostImgSection({ imgUrl }: ImgProps) {
-  const router = useRouter();
   return (
     <Image
       role="presentation"
-      onClick={() => router.push(`/post/${id}`)}
       className={styles.postImg}
       width={385}
       height={287}
