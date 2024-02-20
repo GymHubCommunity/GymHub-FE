d221e403b218e256adf65a3df3e54de94ef8e754
oauch <lgm9515@naver.com>
2024-02-20 15:19:05 +0900

    feat: 게시글 관련 타입

---
+++ src/types/GetPost.ts
@@ -0,0 +1,43 @@
+interface WriterInfoProps {
+  writerId: number;
+  nickname: string;
+  email: string;
+  profileUrl: string;
+}
+
+interface PostProps {
+  posts: {
+    postId: number;
+    writerInfo: WriterInfoProps;
+    content: string;
+    imageUrl?: string;
+    registeredAt: string;
+  }[];
+  hasNext: boolean;
+}
+
+interface GetPostItemsProps {
+  pages: PostProps[];
+  pageParams: number[];
+}
+
+interface GetPostDetailProps {
+  postId: number;
+  writerInfo: {
+    writerId: number;
+    nickname: string;
+    email: string;
+    profileUrl: string;
+  };
+  content: string;
+  imageUrls?: [string | null];
+  hashtags?: [string | null];
+  registeredAt: string;
+}
+
+export type {
+  GetPostDetailProps,
+  GetPostItemsProps,
+  PostProps,
+  WriterInfoProps,
+};
