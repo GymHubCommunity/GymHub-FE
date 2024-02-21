e46f24bed362676daafc8a986e8f272e15abe557
hanseulhee <3021062@gmail.com>
2024-02-21 15:29:39 +0900

    [#189] feat: useCheckNickName prop 변경

--- src/hooks/useCheckNickName.ts
+++ src/hooks/useCheckNickName.ts
@@ -1,22 +1,21 @@
 import { isCheckNameAtom } from '@/components/molecules/ResignNickName';
-import { postProfile } from '@/constants/MockData';
 import { useSetAtom } from 'jotai';
 import { ChangeEvent, useEffect, useState } from 'react';
 
-// TODO: postProfile.name 임식 mock데이터라서 변경 필요
-function useCheckNickName() {
+function useCheckNickName(nickname: string) {
   const [nickName, setNickName] = useState('');
   const setIsCheckName = useSetAtom(isCheckNameAtom);
 
   const handleCheckNickName = (e: ChangeEvent<HTMLInputElement>) => {
     setNickName(e.target.value);
-    if (e.target.value === postProfile.name) {
+    if (e.target.value === nickname) {
       setIsCheckName(false);
     }
   };
 
+  console.log(nickName)
   useEffect(() => {
-    if (nickName !== postProfile.name) setIsCheckName(true);
+    if (nickName !== nickname) setIsCheckName(true);
   }, [nickName]);
 
   return { handleCheckNickName };
