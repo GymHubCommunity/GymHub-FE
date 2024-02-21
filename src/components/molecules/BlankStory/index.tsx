153cd91eb998a12679be4cef4d978c4bbfea8958
hanseulhee <3021062@gmail.com>
2024-02-21 16:52:20 +0900

    [#198] feat: framer-motion 전역 적용

--- src/components/molecules/BlankStory/index.tsx
+++ src/components/molecules/BlankStory/index.tsx
@@ -1,11 +1,19 @@
 import styles from '@/components/molecules/BlankStory/BlankStory.module.scss';
+import { defaultFadeInUpVariants } from '@/constants/motion';
+import { m } from 'framer-motion';
 
 function BlankStory() {
   return (
-    <div className={styles.wrapper}>
+    <m.div
+      variants={defaultFadeInUpVariants}
+      initial="initial"
+      animate="animate"
+      exit="exit"
+      className={styles.wrapper}
+    >
       <p className={styles.text}>오늘 아무도 운동을 하지 않았어요!</p>
       <p className={styles.boldText}>#오운완 으로 운동을 인증해보세요.</p>
-    </div>
+    </m.div>
   );
 }
 
