a1f32571981699ac19d87244426212b00a2231ae
miniposi <rndtn6851@gmail.com>
2024-02-22 02:36:12 +0900

    [#181] feat: checklist slide 생성

--- src/components/atoms/CheckList/index.tsx
+++ src/components/atoms/CheckList/index.tsx
@@ -1,17 +1,23 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import styles from '@/components/atoms/CheckList/CheckList.module.scss';
 import CheckboxSvg from '@/assets/icons/CheckboxSvg';
 import BlankboxSvg from '@/assets/icons/BlankboxSvg';
+import useSelectedPart from '@/hooks/useSelectedPart';
 
 function CheckList({ children }) {
   const [isSelected, setIsSelected] = useState(false);
+  const { handleChecklist, selectedMachines } = useSelectedPart();
 
-  const handleSelected = () => {
-    setIsSelected(!isSelected);
+  const handleSelected = (name) => {
+    setIsSelected(handleChecklist(name));
   };
 
+  useEffect(() => {
+    if (selectedMachines.includes(children)) setIsSelected(true);
+  }, []);
+
   return (
-    <div className={styles.wrapper} onClick={handleSelected}>
+    <div className={styles.wrapper} onClick={() => handleSelected(children)}>
       {isSelected ? <CheckboxSvg /> : <BlankboxSvg />}
       {children}
     </div>
