5674b7ac8371494640047cfda3fffc2931d8c9ad
oauch <lgm9515@naver.com>
2024-02-22 18:00:20 +0900

    [#214] feat: 댓글 드래그로 삭제 구현

--- src/components/molecules/Comment/index.tsx
+++ src/components/molecules/Comment/index.tsx
@@ -1,65 +1,85 @@
-import LikeSvg from '@/assets/icons/LikeSvg';
+import { TrashCanSvg } from '@/assets/icons/TrashCanSvg';
 import LikeButton from '@/components/atoms/Button/LikeButton';
 import ProfileImg from '@/components/atoms/ProfileImg';
 import Text from '@/components/atoms/Text';
 import styles from '@/components/molecules/Comment/Comment.module.scss';
 import useLiked from '@/hooks/useLiked';
-import { usePathname } from 'next/navigation';
+import { motion, useMotionValue } from 'framer-motion';
+import { useEffect, useState } from 'react';
 
 interface CommentProps {
-  comment: {
-    id: number;
-    imgUrl: string;
-    userName: string;
-    date: string;
-    comment: string;
-  }[];
+  id: number;
+  imgUrl: string;
+  userName: string;
+  date: string;
+  comment: string;
 }
 
-function Comment({ comment }: CommentProps) {
+function Comment({ id, imgUrl, userName, date, comment }: CommentProps) {
   const { isLike, countLike, handleLike } = useLiked();
-  const pathName = usePathname();
 
-  if (pathName === '/') {
-    return (
-      <div key={comment[0].id} className={styles.wrapper}>
-        <ProfileImg imgUrl={comment[0].imgUrl} size={34} />
-        <div className={styles.inWrapper}>
-          <div className={styles.userWrapper}>
-            <Text post="commentUserName">{comment[0].userName}</Text>
-            <Text post="commentDate">{comment[0].date}</Text>
-          </div>
-          <Text post="comment">{comment[0].comment}</Text>
+  const itemX = useMotionValue(0);
+  const [isDeleteShow, setIsDeleteShow] = useState(false);
+  const deleteAnimateState = isDeleteShow ? 'appear' : 'disappear';
+
+  useEffect(() => {
+    itemX.on('change', (v) => {
+      const isOverThreshold = v < -64 / 2;
+
+      setIsDeleteShow(isOverThreshold);
+    });
+  }, [itemX]);
+
+  return (
+    <motion.div
+      key={id}
+      className={styles.wrapper}
+      style={{
+        x: itemX,
+      }}
+      drag="x"
+      dragConstraints={{
+        left: -75,
+        right: 0,
+      }}
+      dragElastic={0.1}
+      whileTap={{ cursor: 'grabbing' }}
+    >
+      <ProfileImg imgUrl={imgUrl} size={34} />
+      <div className={styles.inWrapper}>
+        <div className={styles.userWrapper}>
+          <Text post="commentUserName">{userName}</Text>
+          <Text post="commentDate">{date}</Text>
         </div>
-        <LikeButton
-          type="comment"
-          isLike={isLike}
-          handleLike={handleLike}
-          count={countLike}
-        />
+        <Text post="comment">{comment}</Text>
       </div>
-    );
-  } else {
-    return (
-      <>
-        {comment.map((val) => (
-          <div key={val.id} className={styles.wrapper}>
-            <ProfileImg imgUrl={val.imgUrl} size={34} />
-            <div className={styles.inWrapper}>
-              <div className={styles.userWrapper}>
-                <Text post="commentUserName">{val.userName}</Text>
-                <Text post="commentDate">{val.date}</Text>
-              </div>
-              <Text post="comment">{val.comment}</Text>
-            </div>
-            <button onClick={handleLike}>
-              <LikeSvg type={isLike} />
-            </button>
-          </div>
-        ))}
-      </>
-    );
-  }
+      <motion.button
+        className={styles.deleteButton}
+        initial="disappear"
+        animate={deleteAnimateState}
+        variants={{
+          appear: { opacity: 1 },
+          disappear: { opacity: 0 },
+        }}
+      >
+        <motion.p
+          className={styles.deleteLabel}
+          variants={{
+            appear: { scale: 1 },
+            disappear: { scale: 0 },
+          }}
+        >
+          <TrashCanSvg />
+        </motion.p>
+      </motion.button>
+      <LikeButton
+        type="comment"
+        isLike={isLike}
+        handleLike={handleLike}
+        count={countLike}
+      />
+    </motion.div>
+  );
 }
 
 export default Comment;
--- src/components/molecules/PostArticle/index.tsx
+++ src/components/molecules/PostArticle/index.tsx
@@ -1,16 +1,18 @@
 import PostProfile from '@/components/molecules/PostProfile';
 
 import Post from '@/components/molecules/Post';
-import { commentCountAtom } from '@/hooks/atoms';
+import { comment } from '@/constants/MockData';
 import { WriterInfoProps } from '@/types/GetPost';
-import { useAtomValue } from 'jotai';
 import { usePathname } from 'next/navigation';
+import Comment from '../Comment';
+import Reaction from '../Reaction';
 
 interface PostArticleProps {
   postId: number;
   userInfo: WriterInfoProps;
   content: string;
   imageUrl: string | Array<string> | null;
+  registeredAt: string;
 }
 
 function PostArticle({
@@ -18,19 +20,40 @@ function PostArticle({
   userInfo,
   content,
   imageUrl,
+  registeredAt,
 }: PostArticleProps) {
-  const commentCount = useAtomValue(commentCountAtom);
+  // const commentCount = useAtomValue(commentCountAtom);
   const pathName = usePathname();
 
   return (
     <>
       <PostProfile type={'default'} postProfile={userInfo} />
       <Post postId={postId} content={content} imageUrl={imageUrl} />
-      {/* <Reaction /> */}
+      <Reaction registeredAt={registeredAt} />
       {/* {pathName !== '/' && (
         <Text post="commentCount">댓글 {commentCount}개</Text>
       )} */}
-      {/* <Comment comment={comment} /> */}
+      <div />
+      {pathName === '/' ? (
+        <Comment
+          id={comment[0].id}
+          imgUrl={comment[0].imgUrl}
+          userName={comment[0].userName}
+          date={comment[0].date}
+          comment={comment[0].comment}
+        />
+      ) : (
+        comment.map((val) => (
+          <Comment
+            key={val.id}
+            id={val.id}
+            imgUrl={val.imgUrl}
+            userName={val.userName}
+            date={val.date}
+            comment={val.comment}
+          />
+        ))
+      )}
     </>
   );
 }
