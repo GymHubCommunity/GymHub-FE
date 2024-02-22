da7c4873b7966e2cbb73da9ac41e0928b9bf72bf
남궁수영 <sp38013@naver.com>
2024-02-22 21:01:23 +0900

    [#201] feat : 게시글 삭제 , 업데이트 API

--- src/apis/post.ts
+++ src/apis/post.ts
@@ -1,5 +1,12 @@
 import { instance } from '@/apis';
 
+export interface postProps {
+  postId?: string;
+  content?: string;
+  imageUrls?: string[];
+  hashtags?: string[];
+  work?: 'add' | 'update';
+}
 export interface submitPostProps {
   content?: string;
   imageUrls?: string[];
@@ -8,7 +15,17 @@ export interface submitPostProps {
 
 async function submitPost(param: submitPostProps) {
   const response = await instance.post('/posts', JSON.stringify(param));
+  return response.status;
+}
+
+async function updatePost(id: string, param: submitPostProps) {
+  const response = await instance.put(`/posts/${id}`, JSON.stringify(param));
+  return response.status;
+}
+
+async function deletePost(id: number) {
+  const response = await instance.delete(`/posts/${id}`);
   return response.data;
 }
 
-export { submitPost };
+export { submitPost, updatePost, deletePost };
