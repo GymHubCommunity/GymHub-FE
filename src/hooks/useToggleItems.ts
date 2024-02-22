690bcfd79560146abd51b5be14db218926a0ff51
남궁수영 <sp38013@naver.com>
2024-02-22 21:07:05 +0900

    [#201] feat :게시글 수정 토글 이벤트 추가

--- src/hooks/useToggleItems.ts
+++ src/hooks/useToggleItems.ts
@@ -27,7 +27,7 @@ function useToggleItems({ type, id }: ToggleMenuProp) {
     // TODO: 여기에 토글 메뉴에 관련한 기능 추가해주시면 됩니다.
     switch (item) {
       case POST_UPDATE:
-        router.push(`post/${id}`);
+        router.push(`post/${id}/edit`);
         break;
       default:
         showModal();
