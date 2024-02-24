54a02e0b398fa5cfbe871180afd34b90acb0c214
hanseulhee <3021062@gmail.com>
2024-02-25 03:55:13 +0900

    [#220] feat: 로고 적용

--- src/components/molecules/LoginInfo/index.tsx
+++ src/components/molecules/LoginInfo/index.tsx
@@ -1,6 +1,10 @@
-import LogoSvg from '@/assets/icons/LogoSvg';
+import LoginLogoSvg from '@/public/icons/LoginLogo.svg';
 import Text from '@/components/atoms/Text';
 import styles from '@/components/molecules/LoginInfo/LoginInfo.module.scss';
+import Image from 'next/image';
+import { m } from 'framer-motion';
+import { defaultFadeInUpVariants, fadeInUpVariants } from '@/constants/motion';
+import LogoSvg from '@/assets/icons/LogoSvg';
 
 function LoginInfo() {
   return (
@@ -9,6 +13,15 @@ function LoginInfo() {
       <Text onBoarding="intro">
         친구들과 같이하는 운동 얘기{'\n'}지금 시작해볼까요?
       </Text>
+      <m.div
+        className={styles.imgWrapper}
+        variants={defaultFadeInUpVariants}
+        initial="initial"
+        animate="animate"
+        exit="exit"
+      >
+        <Image src={LoginLogoSvg} alt="로고" />
+      </m.div>
     </div>
   );
 }
