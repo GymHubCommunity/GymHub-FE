68bd624073f35553e2ddd6fe22adff04d956dbf1
hanseulhee <3021062@gmail.com>
2024-02-25 16:57:45 +0900

    [#189] feat: 회원 탈퇴 로직

--- src/components/organisms/ResignSection/index.tsx
+++ src/components/organisms/ResignSection/index.tsx
@@ -4,15 +4,18 @@ import ResignTwinButton from '@/components/molecules/ResignTwinButton';
 import BackButtonHeader from '@/components/organisms/Header/BackButtonHeader';
 
 import styles from '@/components/organisms/ResignSection/ResignSection.module.scss';
+import useCheckNickName from '@/hooks/useCheckNickName';
 
 function ResignSection() {
+  const { handleCheckNickName, memberId } = useCheckNickName();
+
   return (
     <>
       <BackButtonHeader pageName="회원 탈퇴" />
       <div className={styles.inWrapper}>
         <ResignText />
-        <ResignNickName />
-        <ResignTwinButton />
+        <ResignNickName onChange={handleCheckNickName} />
+        <ResignTwinButton memberId={memberId} />
       </div>
     </>
   );
--- src/hooks/useResignButton.ts
+++ src/hooks/useResignButton.ts
@@ -1,3 +1,4 @@
+import { deleteUser } from '@/apis/user/register';
 import { isCheckNameAtom } from '@/components/molecules/ResignNickName';
 import { useAtomValue } from 'jotai';
 import { useRouter } from 'next/navigation';
@@ -8,10 +9,12 @@ function useResignButton() {
   const handleCancelClick = () => {
     router.push('/');
   };
-  // TODO: delete 클릭은 닉네임과 일치할 때 활성화 되도록
-  const handleDeleteClick = () => {
+
+  const handleDeleteClick = async (memberId: number) => {
+    await deleteUser(memberId);
     router.push('/');
   };
+
   return { handleCancelClick, handleDeleteClick, isCheckName };
 }
 
