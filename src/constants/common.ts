bee78e590cd25f6e8e9ec87f35a4e182edfd98f6
oauch <lgm9515@naver.com>
2024-02-25 21:49:49 +0900

    [#214] refactor: BASE_URL 경로 수정

--- src/constants/common.ts
+++ src/constants/common.ts
@@ -1,6 +1,6 @@
 //TODO: url 수정
 //const BASE_URL = 'http://localhost:3000';
-const BASE_URL = 'https://3.37.123.52/';
+const BASE_URL = 'https://3.37.123.52';
 //const BASE_URL = 'http://localhost:8080';
 
 //TODO: AWS S3 임시 URL ,BUCKET_NAME 배포되면 수정 해야함.
