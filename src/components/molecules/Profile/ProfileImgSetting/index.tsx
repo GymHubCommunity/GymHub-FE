8b8bebd1b1fef97682772daa29d4801ed29fe126
hanseulhee <3021062@gmail.com>
2024-02-25 17:15:26 +0900

    [#189] feat: profileImg setting 구조 변경

--- src/components/molecules/Profile/ProfileImgSetting/index.tsx
+++ src/components/molecules/Profile/ProfileImgSetting/index.tsx
@@ -1,53 +1,45 @@
 import styles from '@/components/molecules/Profile/ProfileImgSetting/ProfileImgSetting.module.scss';
 import CameraSvg from '@/assets/icons/CameraSvg';
 import ProfileImgSvg from '@/assets/icons/ProfileImgSvg';
-import { ChangeEvent, useEffect, useState } from 'react';
+import { ChangeEvent, useState } from 'react';
 import Image from 'next/image';
-import useImageUpload from '@/hooks/useImageUpload';
-import { ImgUploadButtonProps } from '@/types/image';
 
-function ProfileImgSetting({ prop, onImageChange }: ImgUploadButtonProps) {
-  const { file, setFile, handleSetPresignedURL } = useImageUpload();
-  const [image, setImage] = useState('');
+function ProfileImgSetting(props: any) {
+  const [previewImage, setPreviewImage] = useState<string>('');
 
-  const handleImageChange = (e: ChangeEvent<HTMLInputElement>) => {
-    if (!e.target.files) return;
-    const file = e.target.files[0];
-    const reader = new FileReader();
-    reader.readAsDataURL(file);
-    reader.onload = (e: ProgressEvent<FileReader>) => {
-      if (reader.readyState === 2) {
-        setImage(e.target?.result as string);
-        onImageChange(e.target?.result as string);
-      }
-    };
-    e.target.value = '';
-    setFile(file);
-  };
+  const handleChangeFile = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files) {
+      const file = e.target.files?.[0];
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
 
-  useEffect(() => {
-    handlePresignedUrl();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [file]);
-
-  const handlePresignedUrl = async () => {
-    if (!file) return;
-    await handleSetPresignedURL(file);
+      reader.onload = (e) => {
+        if (typeof e.target?.result === 'string') {
+          setPreviewImage(e.target.result);
+        }
+      };
+    }
   };
 
   return (
-    <label htmlFor="profileImgInput">
+    <label htmlFor="profileImage">
       <div className={styles.wrapper}>
         <input
-          id="profileImgInput"
+          id="profileImage"
           type="file"
+          accept="image/png, image/jpeg, image/jpg"
           className={styles.cameraInput}
-          onChange={handleImageChange}
-          {...prop}
+          onChange={handleChangeFile}
+          ref={props.inputRef}
         />
         <div className={styles.profileImg}>
-          {image ? (
-            <Image src={image} alt="이미지 미리보기" width={128} height={128} />
+          {previewImage ? (
+            <Image
+              src={previewImage}
+              alt="이미지 미리보기"
+              width={128}
+              height={128}
+            />
           ) : (
             <ProfileImgSvg />
           )}
