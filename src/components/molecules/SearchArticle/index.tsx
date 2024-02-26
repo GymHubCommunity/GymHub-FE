1d6dd99923d10e26c845eddd0f1b5e245138e365
남궁수영 <sp38013@naver.com>
2024-02-27 01:59:41 +0900

    [#224] feat : 전체 탭 페이지

--- src/components/molecules/SearchArticle/index.tsx
+++ src/components/molecules/SearchArticle/index.tsx
@@ -4,15 +4,18 @@ import styles from '@/components/molecules/SearchArticle/SearchArticle.module.sc
 import { filterValueAtom } from '@/hooks/useSearchFilter';
 
 import { atom, useAtomValue } from 'jotai';
+import { usePathname } from 'next/navigation';
 
 export const totalCountAtom = atom(0);
 function SearchArticle() {
   const filterValue = useAtomValue(filterValueAtom);
   const totalCount = useAtomValue(totalCountAtom);
+  const pathName = usePathname();
   return (
     <div className={styles.wrapper}>
       <Input type="hashTag" />
-      {filterValue.length !== 0 || totalCount > 0 ? (
+      {pathName !== '/alltabs' &&
+      (filterValue.length !== 0 || totalCount > 0) ? (
         <Text post="searched">
           검색 결과 {totalCount ?? filterValue.length}건
         </Text>
--- src/components/organisms/AllTabsSection/index.tsx
+++ src/components/organisms/AllTabsSection/index.tsx
@@ -1,60 +1,10 @@
-import Text from '@/components/atoms/Text';
-import Comment from '@/components/molecules/Comment';
-import SearchArticle from '@/components/molecules/SearchArticle';
-import Tabs from '@/components/molecules/Tabs';
-import styles from '@/components/organisms/AllTabsSection/AllTabsSection.module.scss';
-import { comment } from '@/constants/MockData';
-import { filterValueAtom } from '@/hooks/useSearchFilter';
-import { useAtom } from 'jotai';
-import { usePathname } from 'next/navigation';
-import { useEffect, useState } from 'react';
+import SearchSection from '@/components/organisms/SearchSection';
 
 function AllTabsSection() {
-  const [filterValue, setFilterValue] = useAtom(filterValueAtom);
-  const [isSelected, setIsSelected] = useState('전체');
-
-  const pathName = usePathname();
-
-  const handleClicked = (selectTab: string) => {
-    setIsSelected(selectTab);
-  };
-
-  useEffect(() => {
-    setFilterValue([]);
-  }, []);
-
-  // TODO: 눌린 버튼의 글씨가 나옵니다.
-  console.log(isSelected);
-
   return (
-    <div className={styles.wrapper}>
-      <SearchArticle />
-      {/* TODO: 검색된 결과에 따라 나오는 값 변환 해줘야함 */}
-      {filterValue.length !== 0 ? (
-        <>
-          {/* <PostProfile type={'default'} postProfile={postProfile} /> */}
-          {/* <Post post={post} /> */}
-          {/* <Reaction /> */}
-          {comment.map((val) => (
-            <Comment
-              key={val.id}
-              id={val.id}
-              imgUrl={val.imgUrl}
-              userName={val.userName}
-              date={val.date}
-              comment={val.comment}
-            />
-          ))}
-        </>
-      ) : (
-        <>
-          {pathName === '/alltabs' && <Tabs clicked={handleClicked} />}
-          <div className={styles.noPost}>
-            <Text post="noPost">게시글이 없습니다.</Text>
-          </div>
-        </>
-      )}
-    </div>
+    <>
+      <SearchSection />
+    </>
   );
 }
 
