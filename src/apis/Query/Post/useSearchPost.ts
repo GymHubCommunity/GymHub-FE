8a8986abc3e95896336c1bc591a9ed81a0260c88
oauch <lgm9515@naver.com>
2024-02-27 22:49:43 +0900

    [#235] feat: useSearchPost 훅

---
+++ src/apis/Query/Post/useSearchPost.ts
@@ -0,0 +1,28 @@
+import { useInfiniteQuery } from '@tanstack/react-query';
+import { toast } from 'react-hot-toast';
+import { searchPost } from '../searchPost';
+
+function useSearchPost(keyword: string) {
+  const { data, isError, fetchNextPage, hasNextPage } = useInfiniteQuery({
+    queryKey: ['postsSearch', keyword],
+    queryFn: ({ pageParam }) => searchPost({ pageParam, keyword }),
+    initialPageParam: 0,
+    select: (data) => ({
+      pages: data?.pages.flatMap((page) => page.posts) || [],
+      pageParams: data.pageParams,
+      totalPostCount: data?.pages[0]?.totalPostCount || 0,
+    }),
+    getNextPageParam: (lastPage, pages) => {
+      if (!lastPage.hasNext) {
+        return undefined;
+      }
+      return pages.length;
+    },
+  });
+
+  if (isError) toast.error('게시글 검색을 실패했어요.');
+
+  return { data, fetchNextPage, hasNextPage };
+}
+
+export default useSearchPost;
---
+++ src/apis/Query/searchPost.ts
@@ -0,0 +1,58 @@
+import { instance } from '@/apis';
+import { WriterInfoProps } from '@/types/GetPost';
+
+export interface postProps {
+  postId?: string;
+  content?: string;
+  imageUrls?: string[];
+  hashtags?: string[];
+  work?: 'add' | 'update';
+}
+export interface submitPostProps {
+  content?: string;
+  imageUrls?: string[];
+  hashTags?: string[];
+}
+export interface useSearchPostProp {
+  pageParam: number;
+  keyword: string;
+}
+
+export interface searchPostProps {
+  hasNext: boolean;
+  posts: {
+    postId: number;
+    writerInfo: WriterInfoProps;
+    content: string;
+    imageUrl: string | Array<string> | null;
+    commentCount: number;
+    registeredAt: string;
+  }[];
+  totalPostCount: number;
+}
+
+async function submitPost(param: submitPostProps) {
+  const response = await instance.post('/posts', JSON.stringify(param));
+  return response.data;
+}
+
+async function updatePost(id: string, param: submitPostProps) {
+  const response = await instance.put(`/posts/${id}`, JSON.stringify(param));
+  return response.status;
+}
+
+async function deletePost(id: number) {
+  const response = await instance.delete(`/posts/${id}`);
+  return response.data;
+}
+
+async function searchPost({ pageParam, keyword }: useSearchPostProp) {
+  const response = await instance.get<searchPostProps>(
+    keyword === '전체'
+      ? `/posts/search?page=${pageParam}&size=2`
+      : `/posts/search?hashtag=${keyword}&page=${pageParam}&size=2`,
+  );
+  return response.data;
+}
+
+export { deletePost, searchPost, submitPost, updatePost };
