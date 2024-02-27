a9a2908fedc9f209d093bdb3c71e2812bbc58aca
남궁수영 <sp38013@naver.com>
2024-02-27 20:09:29 +0900

    [#239] refactor : 메인페이지에서 로그인이 안되어있을 경우 로그인페이지로 이동

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,13 @@
 
 import Footer from '@/components/organisms/Footer';
 import MainSection from '@/components/organisms/MainSection';
+import { useRouter } from 'next/navigation';
 
 function Home() {
+  const router = useRouter();
+  if (!localStorage.getItem('accessToken')) {
+    return router.push('/signin');
+  }
   return (
     <>
       <MainSection />
