d851bcfabbadbdac8ce32a0349215c27c66b6953
oauch <lgm9515@naver.com>
2024-03-04 15:34:55 +0900

    [#255] refactor: 스타일 및 기능 수정

--- src/app/alltabs/page.tsx
+++ src/app/alltabs/page.tsx
@@ -1,15 +1,9 @@
 'use client';
 
 import AllTabsSection from '@/components/organisms/AllTabsSection';
-import Footer from '@/components/organisms/Footer';
 
 function AllTabs() {
-  return (
-    <>
-      <AllTabsSection />
-      <Footer />
-    </>
-  );
+  return <AllTabsSection />;
 }
 
 export default AllTabs;
