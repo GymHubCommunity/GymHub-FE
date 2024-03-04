99d9f889d66278abeb8e5539a3b8cc3c17362853
oauch <lgm9515@naver.com>
2024-03-04 15:33:42 +0900

    [#255] feat: follow pending 상태일 때, 팔로우 요청/거절 페이지 API 연동

---
+++ src/app/notification/page.tsx
@@ -0,0 +1,8 @@
+'use client';
+import NotificationSection from '@/components/organisms/NotificationSection';
+
+function Notification() {
+  return <NotificationSection />;
+}
+
+export default Notification;
--- src/components/molecules/FollowList/index.tsx
+++ src/components/molecules/FollowList/index.tsx
@@ -1,4 +1,5 @@
 import {
+  useApproveFollow,
   useDeleteFollow,
   usePostFollow,
   usePostUnfollow,
@@ -7,21 +8,29 @@ import { followButtonAtom } from '@/components/atoms/Button/FollowButton';
 import FollowToggleButton from '@/components/atoms/Button/FollowToggleButton';
 import styles from '@/components/molecules/FollowList/FollowList.module.scss';
 import { useAtomValue } from 'jotai';
+import { usePathname } from 'next/navigation';
 import { useState } from 'react';
 
 interface FollowListProps {
   id: number;
+  followId: number;
   profileUrl: string;
   name: string;
 }
 
-function FollowList({ id, profileUrl, name }: FollowListProps) {
+function FollowList({ id, followId, profileUrl, name }: FollowListProps) {
   const [followingContent, setFollowingContent] = useState('팔로잉');
   const followButton = useAtomValue(followButtonAtom);
-  const { deleteFollow } = useDeleteFollow({ memberId: id });
 
   const { postFollow } = usePostFollow({ memberId: id });
   const { postUnfollow } = usePostUnfollow({ memberId: id });
+
+  const { approveFollow } = useApproveFollow({ followId: followId });
+  const { deleteFollow } = useDeleteFollow({ followId: followId });
+
+  console.log(followId);
+
+  const pathName = usePathname();
 
   const handleFollow = () => {
     if (followingContent === '팔로잉') {
@@ -36,13 +45,27 @@ function FollowList({ id, profileUrl, name }: FollowListProps) {
   return (
     <div className={styles.wrapper}>
       <img src={profileUrl} alt="프로필 이미지" className={styles.img} />
-      <p className={styles.name}>{name}</p>
-      {followButton === 'follower' ? (
-        <FollowToggleButton onClick={deleteFollow}>삭제</FollowToggleButton>
+      <div className={styles.followWrapper}>
+        <p className={styles.follow}>팔로우 요청</p>
+        <p className={styles.name}>{name}</p>
+      </div>
+      {pathName.startsWith('/follow') ? (
+        followButton === 'follower' ? (
+          <FollowToggleButton onClick={deleteFollow}>삭제</FollowToggleButton>
+        ) : (
+          <FollowToggleButton onClick={handleFollow}>
+            {followingContent}
+          </FollowToggleButton>
+        )
       ) : (
-        <FollowToggleButton onClick={handleFollow}>
-          {followingContent}
-        </FollowToggleButton>
+        <div className={styles.buttonWrapper}>
+          <button className={styles.accept} onClick={() => approveFollow()}>
+            수락
+          </button>
+          <button className={styles.deny} onClick={() => deleteFollow()}>
+            거절
+          </button>
+        </div>
       )}
     </div>
   );
