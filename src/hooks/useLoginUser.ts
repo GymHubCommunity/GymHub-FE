124e4afb091f67b0ff31ddaaad3c65bf5ddeb3eb
남궁수영 <sp38013@naver.com>
2024-03-20 01:25:29 +0900

    [#262] feat : 로그인 성공시 유저정보 전역객체 저장

---
+++ src/hooks/useLoginUser.ts
@@ -0,0 +1,36 @@
+import { useAtom } from 'jotai';
+import { atomWithStorage } from 'jotai/utils';
+
+export interface LoginUserProps {
+  email: string;
+  id: number;
+  nickname: string;
+  profileUrl: string;
+}
+
+const loginUserAtom = atomWithStorage<LoginUserProps | null>('loginUser', {
+  email: '',
+  id: 0,
+  nickname: '',
+  profileUrl: '',
+});
+
+function useLoginUser(): [
+  LoginUserProps | null,
+  (userInfo: LoginUserProps | null) => void,
+  () => void,
+] {
+  const [loginUser, setLoginUser] = useAtom(loginUserAtom);
+
+  const initUser = () => {
+    setLoginUser({ email: '', id: 0, nickname: '', profileUrl: '' });
+  };
+
+  const setUser = (userInfo: LoginUserProps | null) => {
+    setLoginUser(userInfo);
+  };
+
+  return [loginUser, setUser, initUser];
+}
+
+export default useLoginUser;
