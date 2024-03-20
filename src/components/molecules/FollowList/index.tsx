ae45e1ca23f653add3e45495e2aa9f9794f78935
oauch <lgm9515@naver.com>
2024-03-21 00:38:50 +0900

    refactor: 다른 사람 프로필일 때 팔로우 팔로우 취소 버튼으로 변경 (수정본)

--- src/components/molecules/FollowList/index.tsx
+++ src/components/molecules/FollowList/index.tsx
@@ -7,18 +7,27 @@ import {
 import { followButtonAtom } from '@/components/atoms/Button/FollowButton';
 import FollowToggleButton from '@/components/atoms/Button/FollowToggleButton';
 import styles from '@/components/molecules/FollowList/FollowList.module.scss';
+import useLoginUser from '@/hooks/useLoginUser';
 import { useAtomValue } from 'jotai';
+import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { useState } from 'react';
 
 interface FollowListProps {
   id: number;
   followId: number;
+  memberId?: number;
   profileUrl: string;
   name: string;
 }
 
-function FollowList({ id, followId, profileUrl, name }: FollowListProps) {
+function FollowList({
+  id,
+  memberId,
+  followId,
+  profileUrl,
+  name,
+}: FollowListProps) {
   const [followingContent, setFollowingContent] = useState('팔로잉');
   const followButton = useAtomValue(followButtonAtom);
 
@@ -27,6 +36,10 @@ function FollowList({ id, followId, profileUrl, name }: FollowListProps) {
 
   const { approveFollow } = useApproveFollow({ followId: followId });
   const { deleteFollow } = useDeleteFollow({ followId: followId });
+
+  const [loginUser] = useLoginUser();
+
+  const isUser = loginUser?.id === memberId;
 
   const pathName = usePathname();
 
@@ -42,22 +55,28 @@ function FollowList({ id, followId, profileUrl, name }: FollowListProps) {
 
   return (
     <div className={styles.wrapper}>
-      <img src={profileUrl} alt="프로필 이미지" className={styles.img} />
+      <Link href={`/members/${id}`}>
+        <img src={profileUrl} alt="프로필 이미지" className={styles.img} />
+      </Link>
       <div className={styles.followWrapper}>
         {pathName === 'notification' && (
-          <p className={styles.follow}>팔로우 요청</p>
+          <p className={styles.follow}>팔로우 수락</p>
         )}
         <p className={styles.name}>{name}</p>
       </div>
-      {pathName.startsWith('/follow') ? (
-        followButton === 'follower' ? (
+      {isUser ? (
+        pathName.startsWith('/follow') &&
+        (followButton === 'follower' ? (
           <FollowToggleButton onClick={deleteFollow}>삭제</FollowToggleButton>
         ) : (
           <FollowToggleButton onClick={handleFollow}>
             {followingContent}
           </FollowToggleButton>
-        )
+        ))
       ) : (
+        <div />
+      )}
+      {pathName === '/notification' && (
         <div className={styles.buttonWrapper}>
           <button className={styles.accept} onClick={() => approveFollow()}>
             수락
